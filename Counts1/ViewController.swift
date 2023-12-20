739de89b7640cd0afe7dc3f578b14cb2fcbf6c9c
Yandex Praktikum <praktikum@yandex.ru>
2023-12-20 15:03:55 +0300

    Initial Commit

---
+++ Counts1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counts1
+//
+//  Created by  Admin on 20.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
